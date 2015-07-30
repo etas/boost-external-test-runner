66a4b75751fb2fa9faddd9b1dc7bc4ac18ff7aa2
David Joseph Bonnici <djbonnici@gmail.com>
2015-07-30 11:33:21 +0200

    added doxygen documentation that was missing in two classes

--- CBoostTestTreeDebugLister.h
+++ CBoostTestTreeDebugLister.h
@@ -15,12 +15,27 @@ namespace boost
 namespace unit_test
 {
 
+/**
+ * @brief CBoostTestTreeLister extension which additionally serializes debug information for test cases.
+ */
 class CBoostTestTreeDebugLister : public CBoostTestTreeLister
 {
 public:
     typedef CBoostTestTreeLister TBase;
 
+	/**
+	 * @brief Constructor. Output stream defaults to std::out.
+	 *
+	 * @param[in] source file-path to the exe/dll module which contains a Boost test framework
+	 */
     explicit CBoostTestTreeDebugLister(const std::string& source);
+	
+	/**
+	 * @brief Constructor
+	 *
+	 * @param[in] source file-path to the exe/dll module which contains a Boost test framework
+	 * @param[in] out a pointer to the output stream which will be used to output the xml result
+	 */
     CBoostTestTreeDebugLister(const std::string& source, std::ostream* out);
 
     virtual ~CBoostTestTreeDebugLister();
@@ -30,13 +45,25 @@ public:
     virtual bool test_suite_start(const ::boost::unit_test::test_suite& testSuite) override;
     virtual void test_suite_finish(const ::boost::unit_test::test_suite& testSuite) override;
 
+	/**
+	 * @brief States whether or not debug information is available for the requested source module
+	 */
     const bool IsDebugInfoAvailable() const
     {
         return m_dllBase > 0;
     };
 
 private:
+	/**
+	 * @brief Common initialisation logic shared between constructors
+	 */
     void Init();
+	
+	/**
+	 * @brief States whether the provided test suite is the master test suite
+	 * @param[in] testSuite the test suite to test
+	 * @brief States whether the provided test suite is the master test suite
+	 */
     bool IsMasterTestSuite(const ::boost::unit_test::test_suite& testSuite) const;
 
 private:
--- CBoostTestTreeLister.h
+++ CBoostTestTreeLister.h
@@ -14,6 +14,10 @@ namespace boost
 namespace unit_test
 {
 
+/**
+ * @brief Boost test_tree_visitor implementation which traverses the test tree and 
+ * 		  generates an xml file of available tests
+ */
 class CBoostTestTreeLister :
     public ::boost::unit_test::test_tree_visitor,
     private ::boost::noncopyable
@@ -21,36 +25,92 @@ class CBoostTestTreeLister :
 public:
     typedef ::boost::unit_test::test_tree_visitor TBase;
 
+	/**
+	 * @brief Constructor. Output stream defaults to std::out.
+	 *
+	 * @param[in] source file-path to the exe/dll module which contains a Boost test framework
+	 */
     explicit CBoostTestTreeLister(const std::string& source);
+	
+	/**
+	 * @brief Constructor
+	 *
+	 * @param[in] source file-path to the exe/dll module which contains a Boost test framework
+	 * @param[in] out a pointer to the output stream which will be used to output the xml result
+	 */
     CBoostTestTreeLister(const std::string& source, std::ostream* out);
 
+	/**
+	 * @brief Getter for the source module file path which contains the boost test framework
+	 */
     inline const std::string& GetSource() const
     {
         return m_source;
     };
 
+	/**
+	 * @brief States whether or not xml pretty printing is enabled
+	 */
     inline bool GetPrettyPrint() const
     {
         return m_prettyPrint;
     }
 
+	/**
+	 * @brief Sets whether or not xml pretty printing is enabled
+	 * 
+	 * @param[in] prettyPrint true to enable pretty print; false to disable pretty printing
+	 */
     void SetPrettyPrint(bool prettyPrint)
     {
         m_prettyPrint = prettyPrint;
     }
 
+	/**
+	 * @brief Destructor
+	 */
     virtual ~CBoostTestTreeLister();
 
     // test tree visitor interface
+	
+	/**
+	 * @brief Visitor method for a Boost test case. Describes the test is an xml element.
+	 */
     virtual void visit(const ::boost::unit_test::test_case& testCase) override;
+	
+	/**
+	 * @brief Visitor method for the start of a Boost test suite.
+	 */
     virtual bool test_suite_start(const ::boost::unit_test::test_suite& testSuite) override;
+	
+	/**
+	 * @brief Visitor method for the end of a Boost test suite.
+	 */
     virtual void test_suite_finish(const ::boost::unit_test::test_suite& testSuite) override;
 
+	/**
+	 * @brief Writes the xml declaration and preamble
+	 * @return the output stream which we are writing to
+	 */
     std::ostream& WriteHeader();
+	
+	/**
+	 * @brief Writes the closing xml elements for the file
+	 * @return the output stream which we are writing to
+	 */
     std::ostream& WriteTrailer();
 
 protected:
+	/**
+	 * @brief Properly sets up tabulation characters if pretty-printing is enabled
+	 * @return the output stream which we are writing to
+	 */
     std::ostream& Tab();
+	
+	/**
+	 * @brief Getter for the current output stream.
+	 * @return the output stream which we are writing to
+	 */
     std::ostream& Out();
 
 private:
